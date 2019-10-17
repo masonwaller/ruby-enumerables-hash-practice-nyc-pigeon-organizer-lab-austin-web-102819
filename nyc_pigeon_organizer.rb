2e9c605a210733b7a4a354ae601a43136a180e7e
Mason Waller <masonwaller805@gmail.com>
2019-10-17 20:04:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,8 +2,9 @@ def nyc_pigeon_organizer(data)
   new_hash={}
   data.each do |character, value| 
     bird = value[0]
-    if new_hash[bird]
-      data
+    if !new_hash[bird]
+      new_hash<< bird
     end
   end
+  new_hash
 end
