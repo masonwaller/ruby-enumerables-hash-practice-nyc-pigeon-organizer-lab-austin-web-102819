74525617f7054cc2332a25de4446971dc1604d22
Mason Waller <masonwaller805@gmail.com>
2019-10-17 20:15:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,8 @@
 def nyc_pigeon_organizer(data)
   new_hash={}
-  data.each do |character, value| 
-    value.do |
+  data.each do |color_gender_live, value| 
+    value.do |stats, names|
+      
     end
   end
   new_hash
