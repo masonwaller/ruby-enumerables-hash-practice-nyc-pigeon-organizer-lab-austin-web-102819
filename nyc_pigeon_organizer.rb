4da997bd95bdcb8395e2f554d7b7d6ec9bc69aeb
Mason Waller <masonwaller805@gmail.com>
2019-10-17 20:22:36 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash={}
+  data.each do |color_gender_live, value| 
+    value.each do |stats, names|
+      names.each do |name|
+        if !new_hash[name]
+          new_hash[name] = {}
+        end
+        if !new_hash[name][color_gender_live]
+          new_hash[name][color_gender_live]=[]
+        end
+      new_hash[name][color_gender_live].push(stats.to_s)    
+    end
+    end
+  end
+  new_hash
 end
